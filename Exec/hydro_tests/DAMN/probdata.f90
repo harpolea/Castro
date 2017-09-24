68130503bc73f915459ae763f50c7f9f28efaba6
Alice Harpole <alice.harpole@soton.ac.uk>
2017-09-24 18:37:11 +0100

    ***sr dam test

--- Exec/hydro_tests/DAMN/probdata.f90
+++ Exec/hydro_tests/DAMN/probdata.f90
@@ -2,8 +2,6 @@ module probdata_module
 
  ! Problem setup data
   use amrex_fort_module, only : rt => amrex_real
-  real(rt)         :: h_in, h_out, damn_rad
-
-  real(rt)          :: radius
+  real(rt)         :: h_in, h_out, damn_rad, radius
 
 end module probdata_module
