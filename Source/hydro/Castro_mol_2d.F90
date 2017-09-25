56725e0cdf7b2452a982e68c706036f2c1a3ceac
Alice Harpole <alice.harpole@soton.ac.uk>
2017-09-25 13:56:27 +0100

    velocity is going in the wrong direction???

--- Source/hydro/Castro_mol_2d.F90
+++ Source/hydro/Castro_mol_2d.F90
@@ -168,6 +168,8 @@ subroutine ca_mol_single_stage(time, &
 
   deallocate(qxm, qxp, qym, qyp)
 
+  write(*,*) flux1(:, 64, UMX)
+
   ! Normalize the species fluxes
   call normalize_species_fluxes(flux1, flux1_lo, flux1_hi, &
                                 flux2, flux2_lo, flux2_hi, &
