995b549c83544ef830034a64f41d72d821d09957
Alice Harpole <alice.harpole@soton.ac.uk>
2018-01-31 14:02:42 +0000

    HSE boundaries

--- Exec/hydro_tests/DAMN/bc_fill_nd.F90
+++ Exec/hydro_tests/DAMN/bc_fill_nd.F90
@@ -17,7 +17,7 @@ contains
     use eos_module, only : eos
     use eos_type_module, only: eos_input_rt, eos_t
     use network, only: nspec
-    use model_parser_module
+    !use model_parser_module
     use prob_params_module, only: dim
 
     use amrex_fort_module, only : rt => amrex_real
@@ -31,8 +31,9 @@ contains
     real(rt), intent(in)         :: delta(3), xlo(3), time
     real(rt), intent(inout)         :: adv(adv_lo(1):adv_hi(1),adv_lo(2):adv_hi(2),adv_lo(3):adv_hi(3),NVAR)
 
-    integer :: i,j,k,q,n,iter
-    real(rt)         :: x,y
+    integer :: i,j,k,q,n,iter, npts
+    real(rt)         :: x
+    real(rt)         :: xs(domlo(1)-2:adv_hi(1)+1)
     real(rt)         :: pres_above,p_want,pres_zone, A
     real(rt)         :: drho,dpdr,temp_zone,eint,X_zone(nspec),dens_zone
 
@@ -55,7 +56,7 @@ contains
        !        YLO
        if ( bc(2,1,n).eq.EXT_DIR .and. adv_lo(2).lt.domlo(2)) then
 
-          ! we are periodic in y -- we should never get here
+          ! we are FOEXTRAP in y -- we should never get here
           call bl_error("ERROR: invalid BC in Prob_nd.f90")
 
        end if
@@ -63,7 +64,7 @@ contains
        !        YHI
        if ( bc(2,2,n).eq.EXT_DIR .and. adv_hi(2).gt.domhi(2)) then
 
-          ! we are periodic in y -- we should never get here
+          ! we are FOEXTRAP in y -- we should never get here
           call bl_error("ERROR: invalid BC in Prob_nd.f90")
 
        end if
@@ -71,7 +72,7 @@ contains
        !        ZLO
        if ( bc(3,1,n).eq.EXT_DIR .and. adv_lo(3).lt.domlo(3)) then
 
-          ! we are periodic in z -- we should never get here
+          ! we are FOEXTRAP in z -- we should never get here
           call bl_error("ERROR: invalid BC in Prob_nd.f90")
 
        end if
@@ -79,10 +80,15 @@ contains
        !        ZHI
        if ( bc(3,2,n).eq.EXT_DIR .and. adv_hi(3).gt.domhi(3)) then
 
-          ! we are periodic in z -- we should never get here
+          ! we are FOEXTRAP in z -- we should never get here
           call bl_error("ERROR: invalid BC in Prob_nd.f90")
 
        end if
+
+       do i= domlo(1)-2, adv_hi(1)+1
+           xs(i) = xlo(1) + delta(1)*dble(i-adv_lo(1))
+       enddo
+       npts = adv_hi(1) - adv_lo(1) + 1
 
        !        XLO
        if ( bc(1,1,n).eq.EXT_DIR .and. adv_lo(1).lt.domlo(1)) then
@@ -98,97 +104,67 @@ contains
                     ! set all the variables even though we're testing on URHO
                     if (n .eq. URHO) then
 
-                       if (interp_BC) then
+                      ! HSE integration to get density, pressure
 
-                          dens_zone = interpolate(x,npts_model,model_r, &
-                               model_state(:,idens_model))
+                      ! initial guesses
+                      dens_zone = adv(i+1,j,k,URHO)
 
-                          temp_zone = interpolate(x,npts_model,model_r, &
-                               model_state(:,itemp_model))
+                      ! temperature and species held constant in BCs
+                      temp_zone = adv(i+1,j,k,UTEMP)
+                      X_zone(:) = adv(i+1,j,k,UFS:UFS-1+nspec)/adv(i+1,j,k,URHO)
 
-                          do q = 1, nspec
-                             X_zone(q) = interpolate(x,npts_model,model_r, &
-                                  model_state(:,ispec_model-1+q))
-                          enddo
+                      ! get pressure in zone above
+                      eos_state%rho = adv(i+1,j,k,URHO)
+                      eos_state%T = adv(i+1,j,k,UTEMP)
+                      eos_state%xn(:) = adv(i+1,j,k,UFS:UFS-1+nspec)/adv(i+1,j,k,URHO)
 
-                       else
+                      call eos(eos_input_rt, eos_state)
 
-                          ! HSE integration to get density, pressure
-
-                          ! initial guesses
-                          dens_zone = adv(i+1,j,URHO)
-
-                          ! temperature and species held constant in BCs
-                          temp_zone = adv(i+1,j,UTEMP)
-                          X_zone(:) = adv(i+1,j,UFS:UFS-1+nspec)/adv(i+1,j,URHO)
-
-                          ! get pressure in zone above
-                          eos_state%rho = adv(i+1,j,URHO)
-                          eos_state%T = adv(i+1,j,UTEMP)
-                          eos_state%xn(:) = adv(i+1,j,UFS:UFS-1+nspec)/adv(i+1,j,URHO)
-
-                          call eos(eos_input_rt, eos_state)
-
-                          eint = eos_state%e
-                          pres_above = eos_state%p
+                      eint = eos_state%e
+                      pres_above = eos_state%p
 
 
-                          converged_hse = .FALSE.
+                      converged_hse = .FALSE.
 
-                          do iter = 1, MAX_ITER
+                      do iter = 1, MAX_ITER
 
-                             ! pressure needed from HSE
-                             p_want = pres_above - &
-                                  delta(1)*0.5e0_rt*(dens_zone + adv(i+1,j,URHO))*g
+                         ! pressure needed from HSE
+                         p_want = pres_above - &
+                              delta(1)*0.5e0_rt*(dens_zone + adv(i+1,j,k,URHO))*g
 
-                             ! pressure from EOS
-                             eos_state%rho = dens_zone
-                             eos_state%T = temp_zone
-                             eos_state%xn(:) = X_zone
+                         ! pressure from EOS
+                         eos_state%rho = dens_zone
+                         eos_state%T = temp_zone
+                         eos_state%xn(:) = X_zone
 
-                             call eos(eos_input_rt, eos_state)
+                         call eos(eos_input_rt, eos_state)
 
-                             pres_zone = eos_state%p
-                             dpdr = eos_state%dpdr
-                             eint = eos_state%e
+                         pres_zone = eos_state%p
+                         dpdr = eos_state%dpdr
+                         eint = eos_state%e
 
-                             ! Newton-Raphson - we want to zero A = p_want - p(rho)
-                             A = p_want - pres_zone
-                             drho = A/(dpdr + 0.5*delta(1)*g)
+                         ! Newton-Raphson - we want to zero A = p_want - p(rho)
+                         A = p_want - pres_zone
+                         drho = A/(dpdr + 0.5*delta(1)*g)
 
-                             dens_zone = max(0.9_rt*dens_zone, &
-                                  min(dens_zone + drho, 1.1_rt*dens_zone))
-
-
-                             ! convergence?
-                             if (abs(drho) < TOL*dens_zone) then
-                                converged_hse = .TRUE.
-                                exit
-                             endif
-
-                          enddo
-
-                          if (.not. converged_hse) call bl_error("ERROR: failure to converge in -Y BC")
-
-                       endif
+                         dens_zone = max(0.9_rt*dens_zone, &
+                              min(dens_zone + drho, 1.1_rt*dens_zone))
 
 
-                       ! velocity
-                       if (zero_vels) then
+                         ! convergence?
+                         if (abs(drho) < TOL*dens_zone) then
+                            converged_hse = .TRUE.
+                            exit
+                         endif
 
-                          ! zero normal momentum causes pi waves to pass through
-                          adv(i,j,UMY) = 0.e0_rt
+                      enddo
 
-                          ! zero transverse momentum
-                          adv(i,j,UMX) = 0.e0_rt
-                          adv(i,j,UMZ) = 0.e0_rt
-                       else
+                      if (.not. converged_hse) call bl_error("ERROR: failure to converge in -Y BC")
 
-                          ! zero gradient velocity
-                          adv(i,j,UMX) = dens_zone*(adv(domlo(1),j,UMX)/adv(domlo(1),j,URHO))
-                          adv(i,j,UMY) = dens_zone*(adv(domlo(1),j,UMY)/adv(domlo(1),j,URHO))
-                          adv(i,j,UMZ) = dens_zone*(adv(domlo(1),j,UMZ)/adv(domlo(1),j,URHO))
-                       endif
+                      ! zero gradient velocity
+                      adv(i,j,k,UMX) = dens_zone*(adv(domlo(1),j,k,UMX)/adv(domlo(1),j,k,URHO))
+                      adv(i,j,k,UMY) = dens_zone*(adv(domlo(1),j,k,UMY)/adv(domlo(1),j,k,URHO))
+                      adv(i,j,k,UMZ) = dens_zone*(adv(domlo(1),j,k,UMZ)/adv(domlo(1),j,k,URHO))
 
                        eos_state%rho = dens_zone
                        eos_state%T = temp_zone
@@ -199,12 +175,12 @@ contains
                        pres_zone = eos_state%p
                        eint = eos_state%e
 
-                       adv(i,j,URHO) = dens_zone
-                       adv(i,j,UEINT) = dens_zone*eint
-                       adv(i,j,UEDEN) = dens_zone*eint + &
-                            0.5e0_rt*(adv(i,j,UMX)**2+adv(i,j,UMY)**2+adv(i,j,UMZ)**2)/dens_zone
-                       adv(i,j,UTEMP) = temp_zone
-                       adv(i,j,UFS:UFS-1+nspec) = dens_zone*X_zone(:)
+                       adv(i,j,k,URHO) = dens_zone
+                       adv(i,j,k,UEINT) = dens_zone*eint
+                       adv(i,j,k,UEDEN) = dens_zone*eint + &
+                            0.5e0_rt*(adv(i,j,k,UMX)**2+adv(i,j,k,UMY)**2+adv(i,j,k,UMZ)**2)/dens_zone
+                       adv(i,j,k,UTEMP) = temp_zone
+                       adv(i,j,k,UFS:UFS-1+nspec) = dens_zone*X_zone(:)
 
                     end if
 
@@ -226,24 +202,32 @@ contains
                     ! set all the variables even though we're testing on URHO
                     if (n .eq. URHO) then
 
-                       dens_zone = interpolate(x,npts_model,model_r, &
-                            model_state(:,idens_model))
+                       ! dens_zone = interpolate(x,npts_model,model_r, &
+                       !      model_state(:,idens_model))
+                       !
+                       ! temp_zone = interpolate(y,npts_model,model_r, &
+                       !      model_state(:,itemp_model))
+                       !
+                       ! do q = 1, nspec
+                       !    X_zone(q) = interpolate(x,npts_model,model_r, &
+                       !         model_state(:,ispec_model-1+q))
+                       ! enddo
 
-                       temp_zone = interpolate(y,npts_model,model_r, &
-                            model_state(:,itemp_model))
+                       dens_zone = interpolate_noparser(x,xlo(1),adv(:,j,k,URHO),npts,i)
+
+                       temp_zone = interpolate_noparser(x,xlo(1),adv(:,j,k,UTEMP),npts,i)
 
                        do q = 1, nspec
-                          X_zone(q) = interpolate(x,npts_model,model_r, &
-                               model_state(:,ispec_model-1+q))
+                          X_zone(q) = interpolate_noparser(x,xlo(1),adv(:,j,k,UFS-1+q),npts,i)
                        enddo
 
 
                        ! extrap normal momentum
-                       adv(i,j,UMY) = max(0.e0_rt,adv(domhi(1),j,UMY))
+                       adv(i,j,k,UMY) = max(0.e0_rt,adv(domhi(1),j,k,UMY))
 
                        ! zero transverse momentum
-                       adv(i,j,UMX) = 0.e0_rt
-                       adv(i,j,UMZ) = 0.e0_rt
+                       adv(i,j,k,UMX) = 0.e0_rt
+                       adv(i,j,k,UMZ) = 0.e0_rt
 
                        eos_state%rho = dens_zone
                        eos_state%T = temp_zone
@@ -254,12 +238,12 @@ contains
                        pres_zone = eos_state%p
                        eint = eos_state%e
 
-                       adv(i,j,URHO) = dens_zone
-                       adv(i,j,UEINT) = dens_zone*eint
-                       adv(i,j,UEDEN) = dens_zone*eint + &
-                            0.5e0_rt*(adv(i,j,UMX)**2+adv(i,j,UMY)**2+adv(i,j,UMZ)**2)/dens_zone
-                       adv(i,j,UTEMP) = temp_zone
-                       adv(i,j,UFS:UFS-1+nspec) = dens_zone*X_zone(:)
+                       adv(i,j,k,URHO) = dens_zone
+                       adv(i,j,k,UEINT) = dens_zone*eint
+                       adv(i,j,k,UEDEN) = dens_zone*eint + &
+                            0.5e0_rt*(adv(i,j,k,UMX)**2+adv(i,j,k,UMY)**2+adv(i,j,k,UMZ)**2)/dens_zone
+                       adv(i,j,k,UTEMP) = temp_zone
+                       adv(i,j,k,UFS:UFS-1+nspec) = dens_zone*X_zone(:)
 
                     end if
 
@@ -295,8 +279,8 @@ contains
     real(rt), intent(in)         :: delta(3), xlo(3), time
     real(rt), intent(inout)         :: adv(adv_lo(1):adv_hi(1),adv_lo(2):adv_hi(2),adv_lo(3):adv_hi(3))
 
-    integer :: i,j,k
-    real(rt)         :: x
+    integer :: i,j,k,npts
+    real(rt)         :: x, xs(adv_lo(1)-1: adv_hi(1)+1)
 
     ! Note: this function should not be needed, technically, but is
     ! provided to filpatch because there are many times in the algorithm
@@ -326,6 +310,11 @@ contains
        call bl_error("We shoundn't be here (zlo denfill)")
     endif
 
+    do i= adv_lo(1)-1, adv_hi(1)+1
+        xs(i) = xlo(1) + delta(1)*dble(i-adv_lo(1))
+    enddo
+    npts = adv_hi(1) - adv_lo(1) + 1
+
 
     !     XLO
     if ( bc(1,1,1).eq.EXT_DIR .and. adv_lo(1).lt.domlo(1)) then
@@ -333,7 +322,8 @@ contains
            do j=adv_lo(2),adv_hi(2)
               do i=adv_lo(1),domlo(1)-1
                  x = xlo(1) + delta(1)*(dble(i-adv_lo(1)) + 0.5e0_rt)
-                 adv(i,j) = interpolate(x,npts_model,model_r,model_state(:,idens_model))
+                 adv(i,j,k) = interpolate_noparser(x,xlo(1),adv(:,j,k),npts,i)
+                 !interpolate(x,npts_model,model_r,model_state(:,idens_model))
               end do
            end do
         end do
@@ -345,7 +335,8 @@ contains
            do j=adv_lo(2),adv_hi(2)
               do i=domhi(1)+1,adv_hi(1)
                  x = xlo(1) + delta(1)*(dble(i-adv_lo(1))+ 0.5e0_rt)
-                 adv(i,j) = interpolate(x,npts_model,model_r,model_state(:,idens_model))
+                 adv(i,j,k) = interpolate_noparser(x,xlo(1),adv(:,j,k),npts,i)
+                 !interpolate(x,npts_model,model_r,model_state(:,idens_model))
               end do
            end do
         end do
