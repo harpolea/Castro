dc4f9da99b1ef705d97b3f1568bab7c876e758d5
Alice Harpole <aliceharpole@gmail.com>
2020-06-09 08:42:26 -0400

    fix python bug

--- .github/workflows/get_release_txt.py
+++ .github/workflows/get_release_txt.py
@@ -10,22 +10,21 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print('No version provided!')
-        return
+    else:
+        gen_version_re = re.compile(r'#\s(\d\d\.\d\d)')
+        this_version_re = re.compile(f'#\s{sys.argv[1]}')
 
-    gen_version_re = re.compile(r'#\s(\d\d\.\d\d)')
-    this_version_re = re.compile(f'#\s{sys.argv[1]}')
-
-    with open('CHANGES.md', 'r') as file:
-        txt = file.read()
-        m = re.search(this_version_re, txt)
-        if m:
-            # find next date
-            m_next = re.search(gen_version_re, txt[m.end():])
-            if m_next:
-                txt = '   ' + txt[m.end():m.end()+m_next.start()].strip()
+        with open('CHANGES.md', 'r') as file:
+            txt = file.read()
+            m = re.search(this_version_re, txt)
+            if m:
+                # find next date
+                m_next = re.search(gen_version_re, txt[m.end():])
+                if m_next:
+                    txt = '   ' + txt[m.end():m.end()+m_next.start()].strip()
+                else:
+                    txt = '   ' + txt[m.end():].strip()
             else:
-                txt = '   ' + txt[m.end():].strip()
-        else:
-            txt = ""
-                
-        print(f'::set-env name=RELEASE_TXT::{txt}')
+                txt = ""
+                    
+            print(f'::set-env name=RELEASE_TXT::{txt}')
